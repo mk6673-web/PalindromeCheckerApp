32e6b4c44eb9cab6ccb175f968755d6788fd2b31
mk6673-web <mk6673@srmist.edu.in>
2026-02-27 16:41:46 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,40 +1,9 @@
-import java.util.Scanner;
-
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        // UC1: Add welcome message module
         System.out.println("--- Welcome to the Palindrome Checker ---");
 
-        System.out.print("Enter a word or phrase: ");
-        String input = scanner.nextLine();
 
-        if (isPalindrome(input)) {
-            System.out.println("It is a palindrome!");
-        } else {
-            System.out.println("It is not a palindrome.");
-        }
-
-        scanner.close();
-    }
-
-    public static boolean isPalindrome(String text) {
-        // Remove non-alphanumeric characters and convert to lowercase
-        String cleaned = text.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-
-        int left = 0;
-        int right = cleaned.length() - 1;
-
-        while (left < right) {
-            if (cleaned.charAt(left) != cleaned.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-
-        return true;
     }
 }
