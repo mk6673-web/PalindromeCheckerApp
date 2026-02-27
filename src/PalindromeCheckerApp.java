54311c44eac8da353716b6f1412f7db8aca164c6
mk6673-web <mk6673@srmist.edu.in>
2026-02-27 16:35:05 +0530

    initial commit

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,35 +3,7 @@ import java.util.Scanner;
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a word or phrase: ");
-        String input = scanner.nextLine();
-
-        if (isPalindrome(input)) {
-            System.out.println("It is a palindrome!");
-        } else {
-            System.out.println("It is not a palindrome.");
-        }
-
-        scanner.close();
     }
 
-    public static boolean isPalindrome(String text) {
-        // Remove non-alphanumeric characters and convert to lowercase
-        String cleaned = text.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-
-        int left = 0;
-        int right = cleaned.length() - 1;
-
-        while (left < right) {
-            if (cleaned.charAt(left) != cleaned.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-
-        return true;
-    }
 }
